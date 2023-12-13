041c088aeaec8b5a6c4852abef518aac9a76326a
Arjuna Satria <arjunasatria1717@gmail.com>
2023-12-13 15:18:22 +0700

    add: php action

--- forms/contact.php
+++ forms/contact.php
@@ -1,23 +1,42 @@
 <?php
-// Sertakan kelas PHP_Email_Form
-include('./assets/vendor/php-email-form/php-email-form.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Buat objek PHP_Email_Form
-$emailForm = new PHP_Email_Form();
+require 'vendor/autoload.php';
 
-// Atur propertinya
-$emailForm->to = 'tujuan@email.com';
-$emailForm->from_name = 'Nama Pengirim';
-$emailForm->from_email = 'pengirim@email.com';
-$emailForm->subject = 'Subjek Email';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Mengambil nilai dari formulir
+    $yourname = $_POST["yourname"];
+    $youremail = $_POST["youremail"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-// Tambahkan pesan ke badan email
-$emailForm->add_message('Isi pesan email disini.', 'Label Konten');
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.mailtrap.io';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'your_username';
+    $mail->Password   = 'your_password';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 2525;
 
-// Kirim email
-if ($emailForm->send()) {
-    echo 'Email berhasil dikirim!';
-} else {
-    echo 'Gagal mengirim email.';
+
+    // Validasi input
+    if (empty($yourname) || empty($youremail) || empty($subject) || empty($message)) {
+        echo "Mohon isi semua field.";
+    } else {
+        // Konfigurasi email
+        $to = "arjunasatria1717@gmail.com";
+        $subject = "Pesan dari $subject";
+        $message = "Nama: $yourname\nEmail: $youremail\nMessage:\n$message";
+        $headers = "From: $youremail";
+
+        // Mengirim email
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Pesan berhasil dikirim. Terima kasih, $yourname!";
+        } else {
+            echo "Gagal mengirim pesan. Silakan coba lagi.";
+        }
+    }
 }
 ?>
