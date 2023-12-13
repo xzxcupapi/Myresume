fafb301c6209823a0ae93accaade8de84c3a2b4f
Arjuna Satria <arjunasatria1717@gmail.com>
2023-12-13 16:13:41 +0700

    Finalize Resume

--- forms/contact.php
+++ forms/contact.php
@@ -11,31 +11,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.mailtrap.io';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'your_username';
-    $mail->Password   = 'your_password';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port       = 2525;
-
-
     // Validasi input
     if (empty($yourname) || empty($youremail) || empty($subject) || empty($message)) {
         echo "Mohon isi semua field.";
     } else {
-        // Konfigurasi email
-        $to = "arjunasatria1717@gmail.com";
-        $subject = "Pesan dari $subject";
-        $message = "Nama: $yourname\nEmail: $youremail\nMessage:\n$message";
-        $headers = "From: $youremail";
+        try {
+            // Konfigurasi PHPMailer
+            $mail = new PHPMailer();
+            $mail->isSMTP();
+            $mail->Host = 'sandbox.smtp.mailtrap.io';
+            $mail->SMTPAuth = true;
+            $mail->Port = 2525;
+            $mail->Username = '1ff91c1f6a8eff';
+            $mail->Password = 'b72e515879fe13';
 
-        // Mengirim email
-        if (mail($to, $subject, $message, $headers)) {
+            // Set pengaturan email
+            $mail->setFrom($youremail, $yourname);
+            $mail->addAddress('arjunasatria1717@gmail.com');
+            $mail->Subject = "Pesan dari $subject";
+            $mail->Body    = "Nama: $yourname\nEmail: $youremail\nMessage:\n$message";
+
+            // Kirim email
+            $mail->send();
             echo "Pesan berhasil dikirim. Terima kasih, $yourname!";
-        } else {
-            echo "Gagal mengirim pesan. Silakan coba lagi.";
+        } catch (Exception $e) {
+            // Pesan kesalahan
+            echo "Gagal mengirim pesan. Error: {$mail->ErrorInfo}";
         }
     }
 }
